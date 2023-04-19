9023fe1547b20063ae688a8b970e07abeedddc98
MOKRANI <127384858+saidmokrani@users.noreply.github.com>
2023-04-19 17:25:28 +0200

    Update Man.java
    ok

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, mon cher Said");
+        System.out.println("Hello, Mon cher Said");
     }
 }
 
